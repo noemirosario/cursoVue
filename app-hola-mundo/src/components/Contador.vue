29b6c4e6596d44fde35d95abf5150e7faee2f355
Noemi R <noemi_1212rosario@hotmail.com>
2024-01-28 22:09:30 -0600

    diseño de los 2 btns

--- app-hola-mundo/src/components/Contador.vue
+++ app-hola-mundo/src/components/Contador.vue
@@ -26,10 +26,19 @@
     <h1>El valor del contador es: {{numero}}</h1>
     <input type="button" value="Incremetar" v-on:click="incrementar" />
     <input type="button" value="Decremetar" :disabled="numero <= 0"  v-on:click="decrementar"/>
-    <input type="button" value="Resetear" @click="resetear"/>
+    <input type="button" value="Resetear" :disabled="numero === 0" @click="resetear"/>
 </template>
 
 <style>
-
+    body{
+        background-color: #AC7D88;
+    }
+    input{
+        background-color: #85586F;
+        border-radius: 20px;
+        margin-left: 20px;
+        border: none;
+        padding: 10px;
+    }
 </style>
 
